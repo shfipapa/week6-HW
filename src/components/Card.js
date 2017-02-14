ff317192aee5c4ef5e1c40b09a28748776f60f40
shfipapa <slim2017@kellogg.northwestern.edu>
2017-02-13 23:57:20 -0600

    homework

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+var React = require('react')
+
+var Card = React.createClass({
+
+  render: function() {
+    return (
+      <div className="col-sm-2">
+        <h1><img className="img-responsive" src={"http://golearntocode.com/images/cards/" + this.props.name + ".png"} /></h1>
+
+      <div />
+    )
+  }
+
+})
+
+module.exports = Card
